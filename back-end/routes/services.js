eea0e595743782e113e16d01c2ea1889057fc202
Keissy04 <keissyrabenjamina@gmail.com>
2024-02-18 21:02:51 +0300

    Ajout CRUD service dans backend

---
+++ back-end/routes/services.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+const { GetService,CreateService, UpdateService, DeleteService } = require('../controllers/serviceController');
+
+/* CRUD service */
+
+router.get('/index', GetService);
+router.get('/create', CreateService);
+router.put('/update/:id', UpdateService);
+router.delete('/delete/:id',DeleteService);
+
+module.exports = router;
