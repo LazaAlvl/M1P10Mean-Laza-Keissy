afd09f00720654eaebcd47eafea2dd086c309233
LazaAlvl <129962523+LazaAlvl@users.noreply.github.com>
2024-02-29 06:25:18 +0300

    Last-commit-boy-Laza

--- front-end/src/app/pages/contact/contact.component.ts
+++ front-end/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  router = inject(Router);
+
+  Services: any[] = [];
+  authService = inject(AuthService);
+  emailForm!:FormGroup;
+  
+  fb = inject(FormBuilder);
+  ngOnInit(): void {
+    this.emailForm = this.fb.group({
+
+      name: [''],
+      subject: [''],
+      email: [''],
+      message: ['']
+      
+     },
+     );
+
+  }
+
+  onSubmit() {
+    this.authService.sendEmail(this.emailForm.value).subscribe(
+      () => {
+        alert("Email envoyé avec succès");
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+  }
 
 }
